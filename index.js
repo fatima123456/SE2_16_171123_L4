16bc865a534110fbe692a6d9f0e249390d3cbd40
Fatima Saghir <fatima.saghir@live.it>
2016-11-20 19:46:27 +0100

    added index.js and employees.tpl

---
+++ index.js
@@ -0,0 +1,32 @@
+//modules
+var express = require('express');
+var bodyParser = require('body-parser');
+var bind = require('bind');
+
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+//set port
+app.set('port', (process.env.PORT || 1337));
+
+app.use('/', function(req,res){
+    //bind to template
+	bind.toFile('employees.tpl', 
+	{
+        //set up parameters
+        name:'tg',
+    }, 
+    function(data) 
+    {
+        //write response
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        //res.end(data);
+    });
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
