785b164df5cf1f82df20a4629c1674e9c0c1d96b
Fatima Saghir <fatima.saghir@live.it>
2016-11-20 17:51:46 +0100

    added script.js and object employee

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+ *@brief object constructor function to be used to create employee object
+ *@param id,name,surname,level,salary; information about the employee
+ */
+function employee(Id,Name,Surname,Level,Salary){
+    this.id=Id;
+    this.name=Name;
+    this.surname=Surname;
+    this.level=Level;
+    this.salary=Salary;
+}
